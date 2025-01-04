8c483e500f36d8c8d5b3d301d41eed2bbde5fc81
Ahmed Belmehnouf <a_belmehnouf@estin.dz>
2025-01-04 04:30:43 +0100

    Learning Next js

---
+++ src/app/products/[productID]/reviews/page.tsx
@@ -0,0 +1,11 @@
+export default function ReviewsList() {
+    return(
+        <>
+        <h1>reviews list</h1>
+        <h2>reviews 1</h2>
+        <h2>reviews 2</h2>
+        <h2>reviews 3</h2>
+
+        </>
+    )
+}
